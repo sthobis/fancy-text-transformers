98d78b5d075e6ec99b4c5fdb746c26ac0c0dd4e7
sthobis <s.thobi.sinaga@gmail.com>
2018-01-30 18:19:26 +0700

    feat: add text transformer component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FancyTextTransformers from './FancyTextTransformers'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Demo</h1>
+        <p>Hover link below</p>
+        <a href='https://github.com/sthobis'>
+          <FancyTextTransformers
+            text='github.com/sthobis'
+            mask='github'
+          />
+        </a>
       </div>
     );
   }
